82ab308bfdb571a8197e3c10502fb73b3719fc1a
schel-d <schel.d.dev@gmail.com>
2023-12-29 18:30:09 +1100

    undo and redo

--- src/components/PlayGame.vue
+++ src/components/PlayGame.vue
@@ -13,6 +13,7 @@ import type { PlayerConfig } from "@/data/game/player-config";
 import {
   GameStateManager,
   LocalGameStateManager,
+  type StateUpdateType,
 } from "@/data/game/state-manager";
 import { onMounted, onUnmounted, ref, type Ref } from "vue";
 
@@ -27,25 +28,48 @@ const gameState = ref(
 const stateManager = ref<GameStateManager<GameStateType>>(
   new LocalGameStateManager(gameState.value),
 );
+const canUndo = ref(stateManager.value.canUndo());
+const canRedo = ref(stateManager.value.canRedo());
 
-function handleStateUpdate(newState: GameStateType) {
+function handleStateUpdate(
+  newState: GameStateType,
+  _updateType: StateUpdateType,
+) {
   gameState.value = newState;
+}
+function handleUndoRedoAvailabilityChange(
+  nowCanUndo: boolean,
+  nowCanRedo: boolean,
+) {
+  canUndo.value = nowCanUndo;
+  canRedo.value = nowCanRedo;
 }
 
 onMounted(() => {
-  stateManager.value.addEventListener("stateupdate", handleStateUpdate);
+  stateManager.value.addStateUpdateListener(handleStateUpdate);
+  stateManager.value.addUndoRedoAvailabilityListener(
+    handleUndoRedoAvailabilityChange,
+  );
 });
+
 onUnmounted(() => {
-  stateManager.value.removeEventListener("stateupdate", handleStateUpdate);
+  stateManager.value.removeStateUpdateListener(handleStateUpdate);
+  stateManager.value.removeUndoRedoAvailabilityListener(
+    handleUndoRedoAvailabilityChange,
+  );
 });
 </script>
 
 <template>
   <p>Play game</p>
+  <button @click="stateManager.requestUndo()" :disabled="!canUndo">Undo</button>
+  <button @click="stateManager.requestRedo()" :disabled="!canRedo">Redo</button>
 </template>
 
 <style scoped lang="scss">
 @use "@/assets/css-template/import" as template;
 
-// TODO: Add your code here.
+button {
+  @include template.button-classic;
+}
 </style>
--- src/data/game/state-manager.ts
+++ src/data/game/state-manager.ts
@@ -1,45 +1,97 @@
 import type { GameState } from "./game";
 
-export type Listener<GameStateType extends GameState<any>> = (
+export type StateUpdateType = "new" | "undo" | "redo";
+
+export type StateUpdateListener<GameStateType extends GameState<any>> = (
   newState: GameStateType,
+  updateType: StateUpdateType,
+) => void;
+export type UndoRedoAvailabilityListener = (
+  canUndo: boolean,
+  canRedo: boolean,
 ) => void;
 
 export abstract class GameStateManager<GameStateType extends GameState<any>> {
   private _state: GameStateType;
-  private readonly _listeners: Listener<GameStateType>[] = [];
+  private _stateUpdateListeners: StateUpdateListener<GameStateType>[] = [];
+  private _undoRedoAvailabilityListeners: UndoRedoAvailabilityListener[] = [];
 
   constructor(initialState: GameStateType) {
     this._state = initialState;
   }
 
-  addEventListener(event: "stateupdate", listener: Listener<GameStateType>) {
-    this._listeners.push(listener);
-  }
-
-  removeEventListener(event: "stateupdate", listener: Listener<GameStateType>) {
-    const index = this._listeners.indexOf(listener);
-    if (index >= 0) {
-      this._listeners.splice(index, 1);
-    }
-  }
-
   protected getState(): GameStateType {
-    if (this._state === null) {
-      throw new Error("State not initialized");
-    }
     return this._state;
   }
+  abstract canUndo(): boolean;
+  abstract canRedo(): boolean;
+  abstract requestUndo(): void;
+  abstract requestRedo(): void;
 
-  protected updateState(newState: GameStateType) {
+  protected updateState(newState: GameStateType, updateType: StateUpdateType) {
     this._state = newState;
-    this._listeners.forEach((listener) => listener(newState));
+    this._stateUpdateListeners.forEach((x) => x(newState, updateType));
+  }
+  protected updateUndoRedoAvailability(
+    canUndo: boolean,
+    canRedo: boolean,
+  ): void {
+    this._undoRedoAvailabilityListeners.forEach((x) => x(canUndo, canRedo));
+  }
+  addStateUpdateListener(handler: StateUpdateListener<GameStateType>): void {
+    this._stateUpdateListeners.push(handler);
+  }
+  removeStateUpdateListener(handler: StateUpdateListener<GameStateType>): void {
+    this._stateUpdateListeners = this._stateUpdateListeners.filter(
+      (x) => x != handler,
+    );
+  }
+  addUndoRedoAvailabilityListener(handler: UndoRedoAvailabilityListener): void {
+    this._undoRedoAvailabilityListeners.push(handler);
+  }
+  removeUndoRedoAvailabilityListener(
+    handler: UndoRedoAvailabilityListener,
+  ): void {
+    this._undoRedoAvailabilityListeners =
+      this._undoRedoAvailabilityListeners.filter((x) => x != handler);
   }
 }
 
 export class LocalGameStateManager<
   GameStateType extends GameState<any>,
 > extends GameStateManager<GameStateType> {
+  private _undoStack: GameStateType[] = [];
+  private _redoStack: GameStateType[] = [];
+
   constructor(initialState: GameStateType) {
     super(initialState);
+  }
+
+  private _pushNewState(newState: GameStateType): void {
+    this._undoStack.push(this.getState());
+    this._redoStack = [];
+    this.updateState(newState, "new");
+    this.updateUndoRedoAvailability(this.canUndo(), this.canRedo());
+  }
+
+  canUndo(): boolean {
+    return this._undoStack.length > 0;
+  }
+  canRedo(): boolean {
+    return this._redoStack.length > 0;
+  }
+  requestUndo(): void {
+    if (this.canUndo()) {
+      this._redoStack.push(this.getState());
+      this.updateState(this._undoStack.pop()!, "undo");
+      this.updateUndoRedoAvailability(this.canUndo(), this.canRedo());
+    }
+  }
+  requestRedo(): void {
+    if (this.canRedo()) {
+      this._undoStack.push(this.getState());
+      this.updateState(this._redoStack.pop()!, "redo");
+      this.updateUndoRedoAvailability(this.canUndo(), this.canRedo());
+    }
   }
 }
