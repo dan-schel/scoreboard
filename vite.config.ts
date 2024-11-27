bac1920cac2a5637b9303eed5f9a31885cb0fc30
dan-schel <me@danschellekens.com>
2024-11-27 19:09:33 +1100

    setup pwa stuffs

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,58 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { resolve } from "node:path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css}"],
+      },
+      includeAssets: [
+        "favicon.ico",
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "space-grotesk.woff2",
+      ],
+      manifest: {
+        name: "Scoreboard",
+        short_name: "Scoreboard",
+        description: "Track scores/serving/faults",
+        background_color: "#1d1e2e",
+        icons: [
+          {
+            src: "favicon-circle-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "favicon-circle-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "favicon-maskable-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "favicon-maskable-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
